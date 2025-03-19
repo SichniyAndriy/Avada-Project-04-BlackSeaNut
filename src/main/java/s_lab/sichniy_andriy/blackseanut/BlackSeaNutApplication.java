1af1a6bff55bde71e12429be068bc8e630358692
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-03-19 19:14:45 +0200

    add config file and main class

---
+++ src/main/java/s_lab/sichniy_andriy/blackseanut/BlackSeaNutApplication.java
@@ -0,0 +1,13 @@
+package s_lab.sichniy_andriy.blackseanut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackSeaNutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackSeaNutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/s_lab/sichniy_andriy/blackseanut/BlackSeaNutApplicationTests.java
@@ -0,0 +1,13 @@
+package s_lab.sichniy_andriy.blackseanut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackSeaNutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
